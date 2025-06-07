7ebec7700470e451d31fb7c9658c5b475b8959c6
Ayush Poddar <ayush069.poddar.iitm@gmail.com>
2025-06-07 22:13:21 +0530

    ok

--- Demo.js
+++ Demo.js
@@ -34,12 +34,82 @@
 
 // console.log(car.brand); //Toyota
 // console.log(car["year"]); //2023
-// delete car.brand;
-// console.log(car.brand); //undefined
+// car.price="1000";
+// // delete car.brand;
+// console.log(car); //undefined
 
-const student = {
-  name: "Ayush",
-  introduce() {
-    return `Hi, I'm ${this.name}`;
-  }
-};
+// function createRectangle()
+// {
+//   return Rectangle = {
+//     length : 1,
+//     breadth: 2,
+
+//     draw(){
+//       console.log("Drawing Rectangle");
+//     }
+//   }
+// }
+
+// let rectObj1 = createRectangle();
+
+// console.log(rectObj1);
+
+// function Rectangle(){
+//     this.length = 1,
+//     this.breath = 2,
+//     this.draw = function(){
+//       console.log("Printing Rectangle");
+//     }
+// }
+
+// let robj = new Rectangle();
+// console.log(robj);
+
+
+// var arr = [1,2,3,4]
+
+// arr.forEach(function(val){
+//   console.log(val + " hello");
+// })
+
+// //1 hello
+// // 2 hello
+// // 3 hello
+// // 4 hello
+
+// var arr = [1,2,3,4]
+
+// var ans = arr.map(function(val){
+//   return val+10;
+// })
+// console.log(ans);
+
+// var arr = [1,2,3,4]
+
+// var ans = arr.filter(function(val){
+//   if(val>=3) {return true;} //Returning true let us put the value back
+//   else return false; //by default
+// })
+
+// console.log(ans);
+
+
+// var arr = [1,2,3,4]
+
+// var ans = arr.find(function(val){
+//   if(val==2) return val;
+// })
+
+// console.log(ans);
+
+// console.log(arr.indexOf(3));
+
+function abc(x, y){
+}
+
+console.log(abc()); //undefined
+console.log(typeof(abc)); //function
+console.log(typeof(abc())); //undefined
+console.log(abc.length); //2
+
+
